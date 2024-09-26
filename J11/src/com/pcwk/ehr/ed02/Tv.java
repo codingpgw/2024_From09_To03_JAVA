505e5c21003227acb33e18ef76189c18fced879c
codingpgw <pgw20000@gmail.com>
2024-09-26 11:52:24 +0900

    2024 국비학원 JAVA class

---
+++ J11/src/com/pcwk/ehr/ed02/Tv.java
@@ -0,0 +1,19 @@
+package com.pcwk.ehr.ed02;
+
+public class Tv {
+	boolean power;	//전원
+	String 	color;	//색상
+	int 	channel; //채널
+	
+	//메서드
+	public void power() {
+		power = !power;
+	}
+	
+	public void channelUp() {
+		channel++;
+	}
+	public void channelDown() {
+		channel--;
+	}
+}
